e50f6cb6b50d24bf9157a2131a617bdbc277e43a
verstands <rabbymbamu@gmail.com>
2023-07-20 12:21:21 +0100

    model mouvement

--- app/Models/mouvement.php
+++ app/Models/mouvement.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\membre;
+use App\Models\mission;
 
 
 class mouvement extends Model
@@ -14,4 +16,12 @@ class mouvement extends Model
         'membre',
         'mission',
     ];
+
+    public function membre(){
+        return $this->belongsTo(membre::class, 'membre');
+    }
+
+    public function mission(){
+        return $this->belongsTo(mission::class, 'mission');
+    }
 }
